742f8e4a4c83499bb7e3c18a1b80b10463ec4fad
yeomyeongwoo <wymstar@naver.com>
2017-01-22 23:35:21 +0900

    사용자 정보를 가져오는 UserArgumentResolver 설정

--- src/main/java/com/silimstudy/config/WebMvcConfig.java
+++ src/main/java/com/silimstudy/config/WebMvcConfig.java
@@ -1,10 +1,14 @@
 package com.silimstudy.config;
 
+import com.silimstudy.auth.UserArgumentResolver;
 import com.silimstudy.auth.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
@@ -18,8 +22,13 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(interceptor)
-                .addPathPatterns("/**")
+                .excludePathPatterns("/user/**")
                 .excludePathPatterns("/public/**")
-                .excludePathPatterns("/user/**");
+                .addPathPatterns("/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new UserArgumentResolver());
     }
 }
