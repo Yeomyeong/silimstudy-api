45518b69047a8151dd7e237286b66c482f4af459
voyagerwoo <voyager.woo@gmail.com>
2017-03-30 02:40:32 +0900

    CORS 속성 설정

--- src/main/java/com/silimstudy/config/WebMvcConfig.java
+++ src/main/java/com/silimstudy/config/WebMvcConfig.java
@@ -5,6 +5,7 @@ import com.silimstudy.auth.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -31,4 +32,12 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new UserArgumentResolver());
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080");
+    }
+
+
 }
--- src/main/java/com/silimstudy/study/persistence/StudyRepository.java
+++ src/main/java/com/silimstudy/study/persistence/StudyRepository.java
@@ -7,8 +7,7 @@ import java.util.List;
 /**
  * Created by yeomyeongwoo on 2017. 1. 6..
  */
-public interface StudyRepository extends JpaRepository<Study, Long>{
+public interface StudyRepository extends JpaRepository<Study, Long> {
     List<Study> findByAdminId(Long adminId);
 
 }
- 
